492e307ed95bfa255c34b009401812e1bf91530d
zeenayouhan <zinayouhan33@gmail.com>
2021-07-03 23:18:46 +0530

    added launch screen

---
+++ FixMe/FixMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FixMe
+//
+//  Created by Zeena Youhan on 2021-07-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FixMe/FixMe/FixMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixMeApp.swift
+//  FixMe
+//
+//  Created by Zeena Youhan on 2021-07-03.
+//
+
+import SwiftUI
+
+@main
+struct FixMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
