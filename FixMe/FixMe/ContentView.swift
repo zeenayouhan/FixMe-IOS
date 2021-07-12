0d420ad5e03d960d6674d4bd477c10b65073ec3e
zeenayouhan <zinayouhan33@gmail.com>
2021-07-13 00:02:01 +0530

    added a changed to home

--- FixMe/FixMe/ContentView.swift
+++ FixMe/FixMe/ContentView.swift
@@ -6,58 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
 
-//struct LabelStyle : ViewModifier {
-//    func body(content : Content) ->  some View{
-//        return content.background(Color.white).foregroundColor(.green).clipShape(Capsule())
-//    }
-//}
-//struct ButtonStyle : ViewModifier {
-//    func body(content : Content) ->  some View{
-//        return content.padding(.horizontal, 30.0).padding(.vertical, 5)
-//    }
-//}
+struct LabelStyle : ViewModifier {
+    func body(content : Content) ->  some View{
+        return content.background(Color.white).foregroundColor(.green).clipShape(Capsule())
+    }
+}
+struct ButtonStyle : ViewModifier {
+    func body(content : Content) ->  some View{
+        return content.padding(.horizontal, 30.0).padding(.vertical, 5)
+    }
+}
 
 
 struct ContentView: View {
     @AppStorage("log_Status") var status = false
     var body: some View {
-//        NavigationView{
-//        VStack {
-//            Spacer()
-//            Text("Choose a language").padding(.top, 180).font(.system(size: 22))
-//            HStack{
-//                Spacer()
-//
-//                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-//                    Text("සිංහල").modifier(ButtonStyle())
-//                }.modifier(LabelStyle())
-//                Spacer()
-//                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-//                    Text("English").modifier(ButtonStyle())
-//                }.modifier(LabelStyle())
-//
-//                Spacer()
-//                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-//                    Text("தமிழ்").modifier(ButtonStyle())
-//                }.modifier(LabelStyle())
-//
-//                Spacer()
-//            }.padding(.top,10)
-//            Spacer()
-//            Button(action: {}, label: {
-//                    NavigationLink(destination: Login()) {
-//                        Text("->")
-//                            .background(Color.yellow).foregroundColor(.white).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/).font(.system(size: 25)).padding(.bottom, 100 )
-//                    }
-//                })
-//
-//            Spacer()
-//
-//        }
-//        .background(Image("back"))
-//        }
-//    }
         ZStack{
             if status{
                 VStack(spacing : 15){
@@ -78,12 +43,47 @@ struct ContentView: View {
             }
             else{
                 NavigationView{
-                    Login()
+                    VStack {
+                                Spacer()
+                                Text("Choose a language").padding(.top, 180).font(.system(size: 22))
+                                HStack{
+                                    Spacer()
+                    
+                                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                                        Text("සිංහල").modifier(ButtonStyle())
+                                    }.modifier(LabelStyle())
+                                    Spacer()
+                                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                                        Text("English").modifier(ButtonStyle())
+                                    }.modifier(LabelStyle())
+                    
+                                    Spacer()
+                                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                                        Text("தமிழ்").modifier(ButtonStyle())
+                                    }.modifier(LabelStyle())
+                    
+                                    Spacer()
+                                }.padding(.top,10)
+                                Spacer()
+                                Button(action: {}, label: {
+                                        NavigationLink(destination: Login()) {
+                                            Text("->")
+                                                .background(Color.yellow).foregroundColor(.white).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/).font(.system(size: 25)).padding(.bottom, 100 )
+                                        }
+                                    })
+                    
+                                Spacer()
+                    
+                            }
+                            .background(Image("back"))
+                            }
+                
+
                 }
             }
         }
 }
-}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
