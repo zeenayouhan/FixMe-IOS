9d2038b7ff70fce10dba0a8a800b9810f5af25c5
zeenayouhan <zinayouhan33@gmail.com>
2021-07-12 12:35:59 +0530

    added number pad

--- FixMe/FixMe/View/Login.swift
+++ FixMe/FixMe/View/Login.swift
@@ -12,57 +12,97 @@ struct Login: View {
     @State private var pcode: String = " 94"
     @StateObject var loginData = LoginViewModel()
     var body: some View {
-        NavigationView{
-        VStack{
-            Text("Let's find a mechanic").frame(width: 370, height: 400, alignment:.bottomLeading).font(.system(size:25))
-            HStack{
-            VStack(alignment: .leading, spacing: 6) {
-               
-            Text("Enter your number")
-                .font(.caption)
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.gray)
-                
-                Text("+ \(loginData.getCountryCode()) \(loginData.phoneNumber)")
-                .font(.title2)
-                .foregroundColor(.black)
-            
-            }
-                
-            Spacer(minLength: 0)
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Text("Continue")
-                        .foregroundColor(.black)
-                        .padding(.vertical,18)
-                        .padding(.horizontal, 38)
-                        .background(Color.yellow)
-                        .cornerRadius(15)
-            }
-            )
-            }
-            
-            Text("Don't have an account? create account                  ")
-            Text("Privacy Policy                                                             ")
-            GeometryReader{
-                reader in
-                VStack{
-                    LazyVGrid(columns: Array(repeating: GridItem(.flexible(),spacing: 20), count:3), spacing: 15){
-                        ForEach(rows,id: \.self){
-                            value in Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                                Text(value)
-                            }).foregroundColor(.blue)
-                        }
+        
+            VStack{
+                Text("Let's find a mechanic").frame(width: 370, height: 350, alignment:.bottomLeading).font(.system(size:20))
+                HStack{
+                    VStack(alignment: .leading, spacing: 6) {
+                        
+                        Text("Enter your number")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                        Text("+ \(loginData.getCountryCode()) \(loginData.phoneNumber)")
+                            .font(.title)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.black)
+                        
+                        
+                    }
+                    
+                    Spacer(minLength: 0)
+                    Button(action: {}) {
+                        Text("Continue")
+                            .foregroundColor(.black)
+                            .padding(.vertical,18)
+                            .padding(.horizontal, 38)
+                            .background(Color.yellow)
+                            .cornerRadius(15)
                     }
                     
                 }
-            }.padding()
-            Spacer()
-        }.background(Image("back")).padding()
+                .padding(8)
+                .background(Color.white)
+                .cornerRadius(15)
+                
+                Text("Don't have an account? create account                  ")
+                Text("Privacy Policy                                                             ")
+                GeometryReader{
+                    reader in
+                    VStack{
+                        LazyVGrid(columns: Array(repeating: GridItem(.flexible(),spacing: 20), count:3), spacing: 15){
+                            ForEach(rows,id: \.self){
+                                value in Button(action: {}) {
+                                    ZStack{
+                                        if value == "delete.left" {
+                                            Image(systemName: value)
+                                                .font(.title)
+                                                .foregroundColor(.black)
+                                            
+                                        }
+                                        else{
+                                            Text(value).font(.title).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.black)
+                                            
+                                        }
+                                    }
+                                    .frame(width: getWidth(frame: reader.frame(in: .global)), height: getHeight(frame: reader.frame(in: .global))).background(Color.white).cornerRadius(10)
+                                }
+                                .disabled(value == "" ? true : false)
+                        }
+                        
+                    }
+                    }.padding()
+                
+                }
+                
+            }.background(Image("back")).ignoresSafeArea(.all, edges: .bottom)
     }
-    }
+        func getWidth(frame: CGRect) -> CGFloat{
+                let width = frame.width
+                
+                let actualWidth = width - 30
+                
+                return actualWidth / 3
+            }
+            
+        func getHeight(frame: CGRect) -> CGFloat{
+                let height = frame.height
+                
+                let actualHeight = height - 75
+                
+                return actualHeight / 4
+            }
+        
+        
+    
 }
+
 var rows = ["1", "2","3","4", "5","6","7","8","9","","0","delete.left"]
 struct Login_Previews: PreviewProvider {
     static var previews: some View {
         Login()
     }
 }
+
+
+
---
+++ FixMe/FixMe/ViewModel/LoginViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewModel.swift
+//  FixMe
+//
+//  Created by Zeena Youhan on 2021-07-10.
+//
+
+import SwiftUI
+
+class LoginViewModel: ObservableObject{
+    @Published var phoneNumber = ""
+    
+    
+    func getCountryCode() -> String{
+        let regionCode = Locale.current.regionCode ?? ""
+        return countries[regionCode] ?? "" 
+    }
+}
+
